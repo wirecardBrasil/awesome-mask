e2adfe2d155f3c8ffe189dbd78c6aa5394fae91d
Caio Incau <caioincau@gmail.com>
2017-01-19 12:26:48 -0200

    initial commit

---
+++ src/awesome-mask.js
@@ -0,0 +1,21 @@
+import VMasker from 'vanilla-masker'
+
+
+let mask = {};
+
+let inputHandler = (ev) => {
+  ev.target.value = mask ? VMasker.toPattern(ev.target.value, mask) : ev.target.value
+}
+
+export default {
+  bind (el, binding) {
+    mask = binding.expression;
+    el.addEventListener('input', inputHandler)
+  },
+  update (el, binding) {
+    mask = binding.expression;
+  },
+  unbind(el) {
+    el.addEventListener('input', inputHandler)
+  }
+}
