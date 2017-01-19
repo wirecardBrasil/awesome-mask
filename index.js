c0b956692c7f2ae1c6ce7bf4a24302c1f28cc149
Caio Incau <caioincau@gmail.com>
2017-01-19 13:45:20 -0200

    use strict'

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import VMasker from 'vanilla-masker'
+
+
+let mask = {};
+
+let inputHandler = (ev) => {
+  ev.target.value = mask ? VMasker.toPattern(ev.target.value, mask) : ev.target.value
+}
+
+export default {
+  bind (el, binding) {
+    mask = binding.expression;
+    el.addEventListener('input', inputHandler)
+  },
+  update (el, binding) {
+    mask = binding.expression;
+  },
+  unbind(el) {
+    el.addEventListener('input', inputHandler)
+  }
+}
