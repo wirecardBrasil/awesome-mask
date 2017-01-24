91f4ade08d6edf4078dfb1838c8bdc2706a5fe43
Caio Incau <caioincau@gmail.com>
2017-01-24 14:26:36 -0200

    single quote and using exctrated variable

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { isCharacterKeyPress } from './is-character-keypress';
 let inputHandler = (ev) => {
   let mask = ev.target.dataset.mask
   let isCharacter = isCharacterKeyPress(ev) && ev.keyCode !== 9;
-  if (isCharacter && ev.target.value.length >= ev.target.dataset.mask.length) {
+  if (isCharacter && ev.target.value.length >= mask.length) {
     ev.preventDefault();
   }
   ev.target.value = mask ? VMasker.toPattern(ev.target.value, mask) : ev.target.value
@@ -17,11 +17,11 @@ let inputHandler = (ev) => {
 export default {
   bind (el, binding) {
     el.dataset.mask = binding.value
-    el.setAttribute("maxlength", el.dataset.mask.length)
+    el.setAttribute('maxlength', el.dataset.mask.length)
     el.addEventListener('keydown', inputHandler)
   },
   unbind(el) {
-    el.removeAttribute("maxlength")
+    el.removeAttribute('maxlength')
     el.removeEventListener('keydown', inputHandler)
   }
 }
