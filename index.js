6fc54b9f6a0d27ec98c4d2a546ab048e7cfa34f5
Caio Incau <caioincau@gmail.com>
2017-01-24 11:18:33 -0200

    limit

--- index.js
+++ index.js
@@ -8,21 +8,27 @@ var _vanillaMasker = require('vanilla-masker');
 
 var _vanillaMasker2 = _interopRequireDefault(_vanillaMasker);
 
+var _isCharacterKeypress = require('./is-character-keypress');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var inputHandler = function inputHandler(ev) {
   var mask = ev.target.dataset.mask;
+  var isCharacter = (0, _isCharacterKeypress.isCharacterKeyPress)(ev) && ev.keyCode !== 9;
+  if (isCharacter && ev.target.value.length >= ev.target.dataset.mask.length) {
+    ev.preventDefault();
+  }
   ev.target.value = mask ? _vanillaMasker2.default.toPattern(ev.target.value, mask) : ev.target.value;
 };
 
 exports.default = {
   bind: function bind(el, binding) {
-    el.dataset.mask = binding.expression;
-    el.setAttribute("maxlength", binding.expression.length);
-    el.addEventListener('input', inputHandler);
+    el.dataset.mask = binding.value;
+    el.setAttribute("maxlength", el.dataset.mask);
+    el.addEventListener('keydown', inputHandler);
   },
   unbind: function unbind(el) {
     el.removeAttribute("maxlength");
-    el.removeEventListener('input', inputHandler);
+    el.removeEventListener('keydown', inputHandler);
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 'use strict'
 
 import VMasker from 'vanilla-masker'
+import { isCharacterKeyPress } from './is-character-keypress';
+
+
 
 let inputHandler = (ev) => {
   let mask = ev.target.dataset.mask
+  let isCharacter = isCharacterKeyPress(ev) && ev.keyCode !== 9;
+  if (isCharacter && ev.target.value.length >= ev.target.dataset.mask.length) {
+    ev.preventDefault();
+  }
   ev.target.value = mask ? VMasker.toPattern(ev.target.value, mask) : ev.target.value
 }
 
 export default {
   bind (el, binding) {
-    el.dataset.mask = binding.expression
-    el.setAttribute("maxlength", binding.expression.length)
-    el.addEventListener('input', inputHandler)
+    el.dataset.mask = binding.value
+    el.setAttribute("maxlength", el.dataset.mask)
+    el.addEventListener('keydown', inputHandler)
   },
   unbind(el) {
     el.removeAttribute("maxlength")
-    el.removeEventListener('input', inputHandler)
+    el.removeEventListener('keydown', inputHandler)
   }
 }
