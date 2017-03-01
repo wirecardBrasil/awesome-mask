7589d9ffdf9829f68407bcbf9bcbe72df05ac0bd
Caio Incau <caioincau@gmail.com>
2017-03-01 15:10:23 -0300

    Apply mask to default value
    Apply mask to default value:
    
    *If is v-model, use the update hook
    *else use the bind.

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var _vanillaMasker = require('vanilla-masker');
 
 var _vanillaMasker2 = _interopRequireDefault(_vanillaMasker);
 
+var _vue = require('vue');
+
+var _vue2 = _interopRequireDefault(_vue);
+
 var _isCharacterKeypress = require('./is-character-keypress');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -31,12 +35,27 @@ var inputHandler = function inputHandler(ev) {
   }, 0);
 };
 
+var applyMaskToDefault = function applyMaskToDefault(el, mask) {
+  var isInputText = el instanceof HTMLInputElement;
+  var inputText = el;
+  if (!isInputText) {
+    inputText = el.querySelector('input');
+  }
+  inputText.value = mask && mask.length > 0 ? _vanillaMasker2.default.toPattern(inputText.value, mask) : inputText.value;
+};
+
 exports.default = {
   bind: function bind(el, binding) {
     if (binding.value.length < 1) return;
     el.dataset.mask = binding.value;
+    applyMaskToDefault(el, binding.value);
     el.setAttribute('maxlength', el.dataset.mask.length);
     el.addEventListener('keydown', inputHandler);
+  },
+  update: function update(el, binding) {
+    // this is only for v-model
+    if (binding.value.length < 1) return;
+    applyMaskToDefault(el, binding.value);
   },
   unbind: function unbind(el, binding) {
     if (binding.value.length < 1) return;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import VMasker from 'vanilla-masker'
+import Vue from 'vue'
 import { isCharacterKeyPress } from './is-character-keypress';
 
 let allowedKeys = [
@@ -24,12 +25,27 @@ let inputHandler = (ev) => {
   }, 0);
 }
 
+let applyMaskToDefault = (el, mask) => {
+  let isInputText = el instanceof HTMLInputElement ;
+  let inputText = el;
+  if(!isInputText){
+    inputText = el.querySelector('input')
+  }
+  inputText.value = mask && mask.length > 0 ? VMasker.toPattern(inputText.value, mask) : inputText.value
+}
+
 export default {
   bind (el, binding) {
     if(binding.value.length < 1) return
     el.dataset.mask = binding.value
+    applyMaskToDefault(el ,binding.value)
     el.setAttribute('maxlength', el.dataset.mask.length)
     el.addEventListener('keydown', inputHandler)
+  },
+  update(el, binding) {
+    // this is only for v-model
+    if(binding.value.length < 1) return
+    applyMaskToDefault(el ,binding.value)
   },
   unbind(el, binding) {
     if(binding.value.length < 1) return
